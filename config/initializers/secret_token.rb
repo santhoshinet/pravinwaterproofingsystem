ae67a4afc3d6bfdf8ecebbb0b188633fad6ddd32
Santhosh <santhoshinet@gmail.com>
2011-06-17 21:49:38 +0530

    my first commit to git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PravinWaterProofingSystem::Application.config.secret_token = '2fa65e8ff44e26b240f217932cfe143594a532f98cbd07cb7e6e0a9aa3b7e030102f563eee188851a3d9ce0fe40c39bea159e74d33a9c4b9749b8d4360df366c'
